e48ce0e4067dca1d7302f70371ca4f2a6bd80382
j.vlahovic <jovana.vlahovic@odt.net>
2024-03-14 14:21:54 +0100

    add table and search city

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import { TextField, Typography } from "@mui/material";
+import citiesData from "./resources/nl.json";
+import { CitiesTable } from "./components/CitiesTable";
+import { useDebouncedValue } from "./utils/useDebouncedValue";
+import { type City, findCitiesByName } from "./utils/city";
+
+const cities: City[] = citiesData.map((city) => ({
+  name: city.city,
+  province: city.admin_name,
+  population: city.population,
+}));
 
 function App() {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebouncedValue(search);
+
+  const citiesToRender = useMemo(
+    () => findCitiesByName(cities, debouncedSearch),
+    [debouncedSearch]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        overflow: "hidden",
+      }}
+    >
+      <header
+        style={{
+          flexShrink: 0,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "1rem",
+        }}
+      >
+        <Typography variant="h4" component="h1">
+          Find Dutch City
+        </Typography>
+        <TextField
+          sx={{ width: "300px" }}
+          label="City name"
+          variant="outlined"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
       </header>
+
+      <main
+        style={{
+          padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          overflow: "hidden",
+        }}
+      >
+        <CitiesTable cities={citiesToRender} />
+      </main>
     </div>
   );
 }
